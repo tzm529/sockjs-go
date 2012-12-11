706079ffe0ce6db69d4d4bea2a762b6f675061c8
fzzbt <juhanipm@gmail.com>
2012-12-11 21:13:00 +0200

    Added test suite for queue.

---
+++ sockjs/queue_test.go
@@ -0,0 +1,29 @@
+package sockjs
+
+import (
+	. "launchpad.net/gocheck"
+)
+
+type QueueSuite struct{
+	q *queue
+}
+
+var _ = Suite(&QueueSuite{})
+
+func (s *QueueSuite) SetUpTest(c *C) {
+	s.q = newQueue()
+}
+
+func (s *QueueSuite) TestQueue(c *C) {
+	c.Check(s.q.pull(), IsNil)
+
+	s.q.push([]byte{'a'})
+	s.q.push([]byte{'b'})
+	s.q.push([]byte{'c'})
+
+	c.Check(s.q.pull(), DeepEquals, []byte{'a'})
+	c.Check(s.q.pull(), DeepEquals, []byte{'b'})
+	c.Check(s.q.pull(), DeepEquals, []byte{'c'})
+	c.Check(s.q.pull(), IsNil)
+}
+
