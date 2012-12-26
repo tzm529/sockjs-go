a345d5a6288b78a1728a31d1723b1656176df0ee
fzzbt <juhanipm@gmail.com>
2012-12-26 10:40:28 +0200

    merged xhr.go, xhrpolling.go, xhrstreaming.go to xhr.go.

--- sockjs/xhr.go
+++ sockjs/xhr.go
@@ -6,6 +6,60 @@ import (
 	"encoding/json"
 )
 
+//* xhrPolling
+
+type xhrPollingProtocol struct{}
+
+func (p xhrPollingProtocol) contentType() string { return "application/javascript; charset=UTF-8" }
+
+func (p xhrPollingProtocol) writeOpen(w io.Writer) (err error) {
+	_, err = w.Write([]byte("o\n"))
+	return
+}
+
+func (p xhrPollingProtocol) writeData(w io.Writer, m ...[]byte) (n int, err error) {
+	n, err = w.Write(frame("", "\n", m...))
+	return
+}
+
+func (p xhrPollingProtocol) writeClose(w io.Writer, code int, m string) {
+	w.Write(cframe("", code, m, "\n"))
+}
+
+//* xhrStreaming
+
+var prelude []byte = make([]byte, 2049)
+
+func init() {
+	for i := 0; i < 2048; i++ {
+		prelude[i] = byte('h')
+	}
+	prelude[2048] = byte('\n')
+}
+
+type xhrStreamingProtocol struct{}
+
+func (p xhrStreamingProtocol) contentType() string { return "application/javascript; charset=UTF-8" }
+
+func (p xhrStreamingProtocol) writePrelude(w io.Writer) (err error) {
+	_, err = w.Write(prelude)
+	return
+}
+
+func (p xhrStreamingProtocol) writeOpen(w io.Writer) (err error) {
+	_, err = w.Write([]byte("o\n"))
+	return
+}
+
+func (p xhrStreamingProtocol) writeData(w io.Writer, m ...[]byte) (n int, err error) {
+	n, err = w.Write(frame("", "\n", m...))
+	return
+}
+
+func (p xhrStreamingProtocol) writeClose(w io.Writer, code int, m string) {
+	w.Write(cframe("", code, m, "\n"))
+}
+
 func xhrSendHandler(h *Handler, w http.ResponseWriter, r *http.Request, sessid string) {
 	header := w.Header()
 	header.Add("Content-Type", "text/plain; charset=UTF-8")
